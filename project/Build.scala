dc38ee9e39d04511931f817e2bcc8204da718cf0
Matthijs <matthijs@plamola.com>
2014-03-03 23:06:13 +0100

    Updated version to beta4

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "sendR"
-  val appVersion      = "1.1-beta3"
+  val appVersion      = "1.1-beta4"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
