2bb383a59db3e15257e52c97cd64d431c21646d6
Matthijs <matthijs@plamola.com>
2014-03-16 19:43:02 +0100

    Update appVersion to 1.1-rc1

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "sendR"
-  val appVersion      = "1.1-beta6"
+  val appVersion      = "1.1-rc1"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
